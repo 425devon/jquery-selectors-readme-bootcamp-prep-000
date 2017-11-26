cbfc00c1a089fcb30743254284c6748c8c0e0d19
Devon Langendoerfer <425devon@gmail.com>
2017-11-26 06:33:55 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   function paragraphSelector(){
     return $('p');
   }
+  function lastImageSelector(){
+    return $('img:last');
+  }
+  function ninjaBabySelector(){
+    return $('#baby-ninja');
+  }
 })// declare your functions here...
